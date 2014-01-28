fede318f93c41c79e55e6fa065815698cff55dc1
Marc Hanheide <marc@hanheide.de>
2014-01-28 08:39:28 +0000

    timezone as field

--- ttparser.py
+++ ttparser.py
@@ -20,51 +20,54 @@ class RoomTTParser:
     _cal = Calendar()
     _query_rooms = 'MC3204'
     _range = 30
+    _tz_london = timezone('Europe/London')
 
     def __init__(self, room='MC3204', range=30):
         self._query_room = room
-	self._range = range
-	self._cal = Calendar()
-	self._cal.add('prodid', '-//University of Lincoln//Room TimeTable ICal Accessor//')
-	self._cal.add('version', '2.0')
+        self._range = range
+        self._cal = Calendar()
+        self._cal.add('prodid', '-//University of Lincoln//Room TimeTable ICal Accessor//')
+        self._cal.add('version', '2.0')
 
-    def get_ical(self):
-        d = date.today()
-        for x in range(0, self._range):
-            self.addDay(d + timedelta(x))
-        return self._cal.to_ical()
 
-    def addDay(self, today):
-        payload = {'Room': self._query_room, 'TTDate': str(today.day) + '/' + str(today.month) + '/' + str(today.year),
-                   'Step': '3',
-                   'Site': 'BR-000'}
-        r = requests.get("http://stafftimetables.lincoln.ac.uk/V2/UL/Reports/RoomTT.asp", data=payload)
-        htmlparser = etree.HTMLParser()
-        tree = etree.parse(StringIO(r.text), htmlparser)
+def get_ical(self):
+    d = date.today()
+    for x in range(0, self._range):
+        self.addDay(d + timedelta(x))
+    return self._cal.to_ical()
 
-        tz_london = timezone('Europe/London')
-#	tmp=tree.getroot().xpath('/html/*/font/table');
-#	print(len(tmp))
-#	print(tmp[0].tag)
-#        print(etree.tostring(tmp[0]))
-	
-        s = tree.xpath('/html/*/font/table/tr/td/table/tr/td/table/tr[2]/td/table[tr[2]/td]')
-        if (len(s) > 0):
-            room = tree.xpath('/html/*/font/table/tr/td/table/tr/td/table/tr[2]/td[1]/font/b/text()')[0]
-            for se in s:
-                event = Event()
-                name = se.xpath('tr[1]/td/font/text()')
-                #print(name[0])
-                event.add('summary', name[0])
-                time = se.xpath('tr[last()]/td/font/text()')
-                #print(time[0])
-                tv = time[0].replace(' ', '').replace(':', '-').split('-')
-                dBegin = datetime(today.year, today.month, today.day, int(tv[0]), int(tv[1]), tzinfo=tz_london)
-                dEnd = datetime(today.year, today.month, today.day, int(tv[2]), int(tv[3]), tzinfo=tz_london)
-                event.add('dtstart', dBegin)
-                event.add('dtend', dEnd)
-                event['location'] = vText(room)
-                self._cal.add_component(event)
+
+def addDay(self, today):
+    payload = {'Room': self._query_room, 'TTDate': str(today.day) + '/' + str(today.month) + '/' + str(today.year),
+               'Step': '3',
+               'Site': 'BR-000'}
+    r = requests.get("http://stafftimetables.lincoln.ac.uk/V2/UL/Reports/RoomTT.asp", data=payload)
+    htmlparser = etree.HTMLParser()
+    tree = etree.parse(StringIO(r.text), htmlparser)
+
+
+    #	tmp=tree.getroot().xpath('/html/*/font/table');
+    #	print(len(tmp))
+    #	print(tmp[0].tag)
+    #        print(etree.tostring(tmp[0]))
+
+    s = tree.xpath('/html/*/font/table/tr/td/table/tr/td/table/tr[2]/td/table[tr[2]/td]')
+    if (len(s) > 0):
+        room = tree.xpath('/html/*/font/table/tr/td/table/tr/td/table/tr[2]/td[1]/font/b/text()')[0]
+        for se in s:
+            event = Event()
+            name = se.xpath('tr[1]/td/font/text()')
+            #print(name[0])
+            event.add('summary', name[0])
+            time = se.xpath('tr[last()]/td/font/text()')
+            #print(time[0])
+            tv = time[0].replace(' ', '').replace(':', '-').split('-')
+            dBegin = datetime(today.year, today.month, today.day, int(tv[0]), int(tv[1]), tzinfo=self._tz_london)
+            dEnd = datetime(today.year, today.month, today.day, int(tv[2]), int(tv[3]), tzinfo=self._tz_london)
+            event.add('dtstart', dBegin)
+            event.add('dtend', dEnd)
+            event['location'] = vText(room)
+            self._cal.add_component(event)
 
 
 #cgi.test()
